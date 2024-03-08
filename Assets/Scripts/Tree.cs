26f2f41b9513b78dd43a3a5ff09541ffef5e333e
Mirekinho <golanmiroslav@gmail.com>
2024-03-08 23:19:19 +0100

    Changed Tree

--- Assets/Scripts/Tree.cs
+++ Assets/Scripts/Tree.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class Tree : MonoBehaviour
 {
+    public Rigidbody2D m_Rigidbody;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,10 +15,12 @@ public class Tree : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     public void DoSomething()
     {
-        Destroy(gameObject);
+        m_Rigidbody.bodyType = RigidbodyType2D.Dynamic;
+        m_Rigidbody.AddForce(transform.up * 1000f, ForceMode2D.Impulse);
     }
+    void OnBecameInvisible() { Destroy(gameObject); }
 }
