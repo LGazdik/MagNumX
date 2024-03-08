2dcebd5c660679e73f3ae5dc9e2e0661137e4404
Ondrator <162739617+Ondrator@users.noreply.github.com>
2024-03-08 23:45:56 +0100

    update

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -6,7 +6,7 @@ public class Parallax : MonoBehaviour
 {
     private float length, startpos;
     [SerializeField] Transform player;
-    float parallaxEffect;
+    [SerializeField] float parallaxEffect;
     // Start is called before the first frame update
     void Start()
     {
