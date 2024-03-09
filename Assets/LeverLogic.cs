ca0ffe33f68510579593031fc06e94b0d2c8e42c
Ondrator <162739617+Ondrator@users.noreply.github.com>
2024-03-09 10:10:25 +0100

    asdfghjkl

---
+++ Assets/LeverLogic.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverLogic : MonoBehaviour
+{
+    private bool canIteract = false;
+    [SerializeField]private bool isFlipped = false;
+    [SerializeField] private Transform pointA;
+    [SerializeField] private Transform pointB;
+    [SerializeField] private Transform platform;
+
+    [SerializeField] private Transform leverTransform;
+    [SerializeField] private float smoothSpeed = .125f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            Interact();
+        }
+
+        if(isFlipped == true)
+        {
+            platform.position = Vector3.MoveTowards(platform.transform.position, pointA.position, Time.deltaTime * smoothSpeed);
+        }
+        if (isFlipped == false)
+        {
+            platform.position = Vector3.MoveTowards(platform.transform.position, pointB.position, Time.deltaTime * smoothSpeed);
+        }
+
+    }
+
+    public void Interact()
+    {
+        if (canIteract)
+        {
+            if (!isFlipped)
+            {
+                leverTransform.Rotate(Vector3.forward, -90);
+                isFlipped = !isFlipped;
+
+            }
+            else
+            {
+                leverTransform.Rotate(Vector3.forward, 90);
+                isFlipped = !isFlipped;
+            }
+            //GFX
+            //posunoutPlatformu
+        }
+    }
+
+    
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        canIteract = true;
+    }
+   
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        canIteract = false;
+    }
+}
---
+++ Assets/Vor.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vor : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 1f;
+    [SerializeField] Rigidbody2D player;
+    private bool isLockedOnBoat = false;
+    bool isOnBoat = false;
+    float x;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        if (isOnBoat)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                isLockedOnBoat = !isLockedOnBoat;
+                player.constraints = isLockedOnBoat ? RigidbodyConstraints2D.FreezeAll : RigidbodyConstraints2D.FreezeRotation;
+            }
+
+            if(isOnBoat && isLockedOnBoat)
+                transform.position += new Vector3(x, 0, 0);
+
+        }
+
+
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision.gameObject.transform.SetParent(transform);
+        isOnBoat = true;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        collision.gameObject.transform.SetParent(null);
+        isOnBoat = false;
+    }
+}
